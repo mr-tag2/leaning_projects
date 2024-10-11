1fb3f5f8861ac96c4fb930a36bffd838097ac08e
mr_tag2 <104317822+mr-tag2@users.noreply.github.com>
2024-10-10 22:57:20 -0700

    Update issue

---
+++ c#/WindowsFormsApplication2/WindowsFormsApplication2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    public class Transaction
+
+    {
+        public string title;
+        public decimal value;
+        public string description;
+    }
+}
---
+++ c#/WindowsFormsApplication2/WindowsFormsApplication2/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form2 : Form
+    {
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Transaction list = new Transaction();
+
+            list.title = textTitle.Text;
+            list.value= Convert.ToDecimal(textPrice.Text);
+            list.description = richText.Text;
+
+            Form1.trans.Add(list);
+            this.Close();
+
+        }
+
+
+    }
+}
