5a9e3652b6f1cdbfe157ea9f98c8f819454e6ff4
mr_tag2 <104317822+mr-tag2@users.noreply.github.com>
2024-10-11 19:23:52 +0330

    WinForms

--- c#/AccWinForms/AccWinForms/Form1.Designer.cs
+++ c#/AccWinForms/AccWinForms/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace AccWinForms
+﻿namespace WindowsFormsApplication2
 {
     partial class Form1
     {
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(216, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(195, 137);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "نمایش تراکنش ها";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(15, 49);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(195, 137);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "افزودن تراکنش";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(427, 219);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- c#/AccWinForms/AccWinForms/Form1.cs
+++ c#/AccWinForms/AccWinForms/Form1.cs
@@ -8,13 +8,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace AccWinForms
+namespace WindowsFormsApplication2
 {
     public partial class Form1 : Form
     {
+        public static List<Transaction> trans = new List<Transaction>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 Form2=new Form2();
+            Form2.Show();
         }
     }
 }
